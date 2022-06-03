f28cd3a487e122f6afdba9aac4cdd0647b832a90
thuythuytiennguyen <thuytienthuytien134@gmail.com>
2022-06-02 21:03:14 -0700

    completed

--- src/DogPics.js
+++ src/DogPics.js
@@ -6,7 +6,7 @@ function DogPics() {
   const [images, setImages] = useState([]);
 
   useEffect(() => {
-    console.log("useEffect");
+    // console.log("useEffect");
     fetch("https://dog.ceo/api/breeds/image/random/3")
       .then((r) => r.json())
       .then((data) => {
